c8aab8fc90e1b73f9e72474938465771dc35eabf
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-02-17 21:37:39 -0500

    fixed anime

--- script/getTopTen.js
+++ script/getTopTen.js
@@ -12,3 +12,5 @@ total = ""
 for (i = 0; i < 10; i++) {
     total += "<div class=\"carousel-cell\"><img class=\"img-fluid imageHover poster mx-2\" src=\"../MEDIA-TRACKER/script/anime/myImage" + i + ".png\"></div>"
 }
+
+$("#animeCarousel").html(total);
